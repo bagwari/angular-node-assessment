40a5dbf0241e958c307211642a5bd860c7328e90
Manish Bagwari <manishbagwari65@gmail.com>
2019-03-31 13:38:30 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HttpClientModule } from '@angular/common/http';
+
+import {
+  MatGridListModule,
+  MatIconModule,
+  MatCardModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { JokeComponent } from './joke/joke.component';
+import { JokeCategoryComponent } from './joke-category/joke-category.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'jokes',
+    component: JokeComponent,
+    data: { title: 'Jokes List' }
+  },
+  {
+    path: 'joke-category/:id',
+    component: JokeCategoryComponent,
+    data: { title: 'Joke Category' }
+  },
+  { path: '',
+    redirectTo: '/jokes',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JokeComponent,
+    JokeCategoryComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(appRoutes),
+    BrowserModule,
+    HttpClientModule,
+    MatGridListModule,
+    MatCardModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
